64657d6f61d0a242e504a0226f2313c95e212049
Renz Marion Bayadog <renzmarionbayadog@outlook.com>
2025-06-05 10:49:22 +0800

    adjust creation of user

--- obj/Debug/net8.0/EventMonitoring.ph.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventMonitoring.ph.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventMonitoring.ph")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9177eb1bd040005e16b3c4604a98aa9acfe56a7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40990bb68880b62a5868abeabb35cc71cdafafa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventMonitoring.ph")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventMonitoring.ph")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
