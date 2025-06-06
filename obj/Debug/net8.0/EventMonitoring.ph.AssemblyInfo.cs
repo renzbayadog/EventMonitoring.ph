3de7a0b7ca77bc423ecd0217cfa66cfefeb98306
Renz Marion Bayadog <renzmarionbayadog@outlook.com>
2025-06-06 18:47:17 +0800

    deploy lates

--- obj/Debug/net8.0/EventMonitoring.ph.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventMonitoring.ph.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventMonitoring.ph")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+237bb84c52eceddacbad593ca5feeeda5407a356")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e7ba1824e983400615047d371a4effaa3f46e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventMonitoring.ph")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventMonitoring.ph")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
