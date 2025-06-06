3f5c10bbc591ea6182c0633320036d2807506614
Renz Marion Bayadog <renzmarionbayadog@outlook.com>
2025-06-06 16:29:51 +0800

    remove session

--- obj/Debug/net8.0/EventMonitoring.ph.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventMonitoring.ph.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventMonitoring.ph")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4db85b355154850597c21df6a739763f6693af02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa912b61bb30cebc6209d7226e5a2e349f0b34a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventMonitoring.ph")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventMonitoring.ph")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
