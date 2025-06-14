afea42b7b0a498bcf2ad0380fcdd88b6a87f2986
Renz Marion Bayadog <renzmarionbayadog@outlook.com>
2025-06-14 16:56:26 +0800

    update necessary fixes

--- obj/Debug/net8.0/EventMonitoring.ph.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventMonitoring.ph.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventMonitoring.ph")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db47d1bec0d4cb6ca7924fef0277c7947b8a7245")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a2054924436c775ff6b2aa8d288f0ca98a4f00c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventMonitoring.ph")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventMonitoring.ph")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
